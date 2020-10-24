efdd76312761d03ba3c7fcd3ca1b1fd4f9b15187
Daniel Urueña <duruena@bancolombia.com.co>
2020-10-24 11:26:44 -0500

    corrección de configuración de jest

---
+++ TIL_backend/jest.config.ts
@@ -0,0 +1,14 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  roots: ['<rootDir>/src'],
+  verbose: true,
+  testMatch: [
+    '**/*.test.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+};
+export default config;
