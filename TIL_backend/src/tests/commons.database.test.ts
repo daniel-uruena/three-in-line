3a31e4b1a84aaa2275d75bf0f59cae871d36fc1c
Daniel Urueña <duruena@bancolombia.com.co>
2020-10-24 20:26:51 -0500

    pruebas unitarias de la conexión a la base de datos

--- TIL_backend/src/tests/commons.database.test.ts
+++ TIL_backend/src/tests/commons.database.test.ts
@@ -1,17 +1,10 @@
 import { IDatabase, MongoDatabase } from '../commons/database'
 
 describe('Game database tests', () => {
-  const mongoDatabase = new MongoDatabase()
 
   test('should return a database instance when connect function is called', () => {
-    const database = mongoDatabase.connect()
+    const mongoDatabase = new MongoDatabase()
 
-    expect(database).toBeInstanceOf(IDatabase)
-  })
-
-  test('should close database connection when close function is called', () => {
-    const result = mongoDatabase.close()
-
-    expect(result).toBeTruthy()
+    expect(mongoDatabase).toBeInstanceOf(IDatabase)
   })
 })
